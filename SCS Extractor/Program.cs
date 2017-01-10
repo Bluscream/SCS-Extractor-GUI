4ea7824a405293d14bd1bf672796c49caa129f77
Bluscream <admin@timo.de.vc>
2017-01-10 15:03:34 +0100

    initial commit

---
+++ SCS Extractor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SCS_Extractor
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            string binary = "\""+AppDomain.CurrentDomain.BaseDirectory + "scs_extractor.exe\"";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\program files (x86)";
+            openFileDialog1.Filter = "SCS Archive (*.SCS)|*.SCS|All files (*.*)|*.*";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "Select SCS Archives";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    string FileExt = System.IO.Path.GetFileName(file);
+                    int idx = FileExt.LastIndexOf('.');
+                    string FileOnly = ""; if (idx >= 0) FileOnly = FileExt.Substring(0, idx);
+                    string PathOnly = System.IO.Path.GetDirectoryName(file);
+                    string cmdline = "\""+file+ "\" \"" + PathOnly+"\\"+FileOnly+"\"";
+                    //MessageBox.Show(binary + " "+cmdline, "SCS Extractor GUI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Process process = new Process() {
+                        StartInfo = new ProcessStartInfo(binary, cmdline) {
+                            WindowStyle = ProcessWindowStyle.Normal, WorkingDirectory = PathOnly
+                        }
+                    };
+                    process.Start();
+                }
+            }
+         }
+    }
+}
